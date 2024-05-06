721056b9508a10edb8869d9a53c1bbcb14e0e894
unknown <lukastyon@gmail.com>
2024-05-06 13:52:46 -0500

    Rename initial function per naming convention, set submit button to initially disabled

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-document.addEventListener("formValidator", function () {
+document.addEventListener("DOMContentLoaded", function () {
+  const submitButton = document.querySelector("input[type='submit']");
+  submitButton.disabled = true;
+
   const form = document.querySelector("form");
   const nameInput = document.getElementById("full-name");
   const emailInput = document.getElementById("email");
   const phoneInput = document.getElementById("phone");
   const messageInput = document.getElementById("message");
-  const submitButton = document.querySelector("input[type='submit']");
 });
