72502e99148d81592c43e4ffaeb7acd754ca8073
unknown <lukastyon@gmail.com>
2024-05-06 13:27:15 -0500

    Create validation function and needed objects

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("formValidator", function () {
+  const form = document.querySelector("form");
+  const nameInput = document.getElementById("full-name");
+  const emailInput = document.getElementById("email");
+  const phoneInput = document.getElementById("phone");
+  const messageInput = document.getElementById("message");
+  const submitButton = document.querySelector("input[type='submit']");
+});
