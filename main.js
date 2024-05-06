d33f02d3ae8f6f7897e6e34a1ab2aa750fbcf339
unknown <lukastyon@gmail.com>
2024-05-06 18:09:51 -0500

    Remove TODOs

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-/**
- * TODO:
- * 2. 'Select' the submit button.
- * 3. Add event listeners to each input field, such that
- *  upon losing focus (`blur` event) on the input fields, we will
- * show the error. For now, we are not validating the input fields.
- */
-
 import { ERRORS, hideError, renderError } from "./src/lib";
 console.log(ERRORS);
 const formEls = [
