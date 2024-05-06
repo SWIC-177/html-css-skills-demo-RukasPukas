ec054b9b719e835882c1f83b82af8991987a8744
unknown <lukastyon@gmail.com>
2024-05-06 15:14:58 -0500

    Create submitBtn object, create event listener that iterates over each element of array

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 /**
  * TODO:
- * 1. 'Select' all of the input fields.
  * 2. 'Select' the submit button.
  * 3. Add event listeners to each input field, such that
  *  upon losing focus (`blur` event) on the input fields, we will
@@ -10,5 +9,14 @@
 const formEls = [
   ...Array.from(document.querySelectorAll("input")),
   document.querySelector("#message"),
+  document.querySelector("#submit-button"),
 ];
 console.log(formEls);
+
+const submitBtn = document.querySelector("button[type='submit']");
+
+formEls.forEach((el) => {
+  el.addEventListener("blur", (e) => {
+    console.log("blur Event", e.target.id);
+  });
+});
